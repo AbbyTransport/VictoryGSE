fa6a9458b06950b7d2d90808892fcdb05a77be4d
AbbyTransport <humberto@abbytransport.com>
2026-05-28 21:10:42 -0600

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBMl4hke6AqJPGch7Y0lTgOOz1dWRq8HDM",
-  authDomain: "victorygse-abby-portal.firebaseapp.com",
-  projectId: "victorygse-abby-portal",
-  storageBucket: "victorygse-abby-portal.firebasestorage.app",
-  messagingSenderId: "119452755981",
-  appId: "1:119452755981:web:7ff69b37daf5bee90acd44",
-  measurementId: "G-1NYPSBEVGM"
-};
